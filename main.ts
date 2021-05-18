8952d9b93df667eb4ea6764d8f0b174b25acb8f4
bigHippoZz <289901948@qq.com>
2021-05-18 20:33:54 +0800

    feat

--- main.ts
+++ main.ts
@@ -1,49 +1,69 @@
-interface EnhanceWebSocketClass {
-  queue: string[];
-  connection: WebSocket | null;
-  path: string;
-  openConnection(): void; // 打开websocket
-  closeConnection(): void; // 关闭websocket
-  onopen(event: Event): void;
-  onmessage(event: MessageEvent): void;
-  onclose(event: CloseEvent): void;
-  onerror(event: Event): void;
-  send(msg: string): void;
+export class Dispatcher<T> {
+  private listeners: Array<(val: T) => void> = [];
+  public subscribe(func: (val: T) => void) {
+    this.listeners.push(func);
+    return () => {
+      const index = this.listeners.indexOf(func);
+      this.listeners.splice(index, 1);
+    };
+  }
+  public dispatch(event: T) {
+    for (let i = 0; i < this.listeners.length; i++) {
+      const listener = this.listeners[i];
+      listener(event);
+    }
+  }
 }
 
-export class EnhanceWebSocket implements EnhanceWebSocketClass {
-  queue: Array<string> = [];
-  connection: WebSocket | null = null; //当前ws的引用
+export class EnhanceWebSocket {
   path: string;
-  private connectionAttempts: number;
+  private queue: Array<string> = [];
+  private connection: WebSocket | null = null; // 当前ws的引用
+  private connectionAttempts: number; // 当前重连次数
   private reconnectionDelay: boolean; // 是否开启重连机制
-  private setTimeoutId!: number | null; // 当前重连定时器id
-  private listener: (event: MessageEvent) => void = () => {};
+  private setTimeoutId!: ReturnType<typeof setTimeout> | null; // 当前重连定时器id
+
+  private onopenDispatcher: Dispatcher<Event>;
+  private onerrorDispatcher: Dispatcher<Event>;
+  private oncloseDispatcher: Dispatcher<CloseEvent>;
+  private onmessageDispatcher: Dispatcher<MessageEvent>;
   constructor(path: string, reconnectionDelay = true) {
     this.path = path;
     this.reconnectionDelay = reconnectionDelay; // 是否开启重连机制
-    this.connectionAttempts = 0; // 当前重连次数
-
-    this.openConnection(); // 尝试打开ws
+    this.connectionAttempts = 0;
+    this.onopenDispatcher = new Dispatcher();
+    this.oncloseDispatcher = new Dispatcher();
+    this.onmessageDispatcher = new Dispatcher();
+    this.onerrorDispatcher = new Dispatcher();
+    this.initialization();
   }
-  openConnection() {
+
+  public openConnection() {
+    if (
+      this.readyState === WebSocket.OPEN ||
+      this.readyState === WebSocket.CONNECTING
+    ) {
+      return;
+    }
+
     try {
       this.connection = new WebSocket(this.path);
-      this.connection.onopen = (val) => this.onopen(val);
-      this.connection.onmessage = (val) => this.onmessage(val);
-      this.connection.onerror = (val) => this.onerror(val);
-      this.connection.onclose = (val) => this.onclose(val);
+      this.connection.onopen = (value) => this.onopenDispatcher.dispatch(value);
+      this.connection.onmessage = (value) =>
+        this.onmessageDispatcher.dispatch(value);
+      this.connection.onerror = (value) =>
+        this.onerrorDispatcher.dispatch(value);
+      this.connection.onclose = (value) =>
+        this.oncloseDispatcher.dispatch(value);
     } catch (error) {
       this.connection = null;
-      console.log("openConnection is error");
+      console.warn("openConnection is error");
     } finally {
       this.connectionAttempts++;
     }
   }
-  /**
-   * 彻底关闭websocket
-   */
-  closeConnection() {
+
+  public closeConnection() {
     if (
       this.connection instanceof WebSocket &&
       this.readyState !== WebSocket.CLOSING &&
@@ -60,62 +80,60 @@ export class EnhanceWebSocket implements EnhanceWebSocketClass {
     }
     this.connectionAttempts = 0;
   }
-  onopen(event: Event) {
-    console.log(event, "websocket connection opened");
-    // 清空队列
-    this.queue.forEach((msg) => this.send(msg));
-    // 清除引用
-    this.queue = [];
+
+  public onmessage(func: (event: MessageEvent) => void) {
+    return this.onmessageDispatcher.subscribe(func);
   }
-  onmessage(event: MessageEvent) {
-    // 返回消息 建议使用回调的方式 this.callback(event)
-    console.log(event.data, "message");
-    this.listener(event);
+  public onopen(func: (event: Event) => void) {
+    return this.onopenDispatcher.subscribe(func);
   }
-  onerror(event: Event) {
-    // 出现连接错误的时候，后面一定会执行关闭事件
-    console.warn(`websocket connection error: ${JSON.stringify(event)}`);
+
+  public onerror(func: (event: Event) => void) {
+    return this.onerrorDispatcher.subscribe(func);
   }
-  onclose({ code, reason, wasClean }: CloseEvent): void {
-    //  close方法的event事件包含 wasClean code reason
-    //  wasClean 表示是否明确的关闭了ws
-    //  code表示后端返回的状态码
-    //  reason 代表后端返回的文本 可以看看有什么
-    console.log(code, "code");
-    console.log(reason, "reason");
-    console.log(wasClean, "wasClean");
-    console.warn(`Closed connection to websocket`);
-    if (this.reconnectionDelay) {
-      this.setTimeoutId = setTimeout(() => this.openConnection(), this.timeout);
-    }
+
+  public onclose(func: (event: CloseEvent) => void) {
+    return this.oncloseDispatcher.subscribe(func);
   }
-  listen(func: (event: MessageEvent) => void) {
-    this.listener = func;
-  }
-  send(msg: string) {
-    if (typeof msg !== "string") throw new Error("msg must be a string");
+
+  public send(msg: string) {
     if (this.readyState === WebSocket.OPEN)
       return (this.connection as WebSocket).send(msg);
     this.queue.push(msg);
   }
+
   // websocket状态码
-  get readyState(): number {
+  public get readyState(): number {
     if (this.connection === null) return WebSocket.CLOSED;
     return this.connection.readyState;
   }
+
   // 重连时间
-  get timeout(): number {
+  private get timeout(): number {
     // 依次增加重连时间 最大值为16秒
     return (Math.pow(2, Math.min(this.connectionAttempts, 5)) - 1) * 1000;
+  }
+  private initialization() {
+    const processWaitingMessage = () => {
+      this.queue.forEach((msg) => this.send(msg));
+      this.queue.length = 0;
+    };
+    const processReconnection = () => {
+      if (!this.reconnectionDelay) return;
+      this.setTimeoutId = setTimeout(() => this.openConnection(), this.timeout);
+    };
+    this.onopenDispatcher.subscribe(processWaitingMessage);
+    this.oncloseDispatcher.subscribe(processReconnection);
   }
   static stringify(target: any): string {
     return JSON.stringify(target);
   }
+
   static parse(string: string) {
     try {
       return JSON.parse(string);
     } catch (error) {
-      console.log(error);
+      return false;
     }
   }
 }
